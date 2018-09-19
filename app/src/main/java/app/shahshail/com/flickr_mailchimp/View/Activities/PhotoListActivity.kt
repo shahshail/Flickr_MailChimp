c0081bbce38bb59c0dc529f38d6ca199922d7bf2
Shail Shah <shahshail9898@gmail.com>
2018-09-18 23:56:21 -0400

    Empty list error handled

--- app/src/main/java/app/shahshail/com/flickr_mailchimp/View/Activities/PhotoListActivity.kt
+++ app/src/main/java/app/shahshail/com/flickr_mailchimp/View/Activities/PhotoListActivity.kt
@@ -2,7 +2,10 @@ package app.shahshail.com.flickr_mailchimp.View.Activities
 
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
+import android.content.Context
 import android.databinding.DataBindingUtil
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
@@ -35,19 +38,20 @@ class PhotoListActivity : AppCompatActivity() {
             override fun onQueryTextSubmit(query: String?): Boolean{
                 if (validateString(query)) {
                     flickrViewModel.loadFlickrPhotos(query!!)
-                    flickrViewModel.setSearchString(query)
                     binding.searchView.closeSearch()
                 }
                 return true
             }
-            override fun onQueryTextChange(newText: String?): Boolean {
-
-                return false
-            }
+            override fun onQueryTextChange(newText: String?): Boolean = false
         })
 
 
         flickrViewModel = ViewModelProviders.of(this).get(PhotoListViewModel::class.java)
+
+        //Observe noResultFound Message
+        flickrViewModel.noResultFoundMessage.observe(this, Observer { message ->
+            if(message != null) Toast.makeText(binding.root.context,message,Toast.LENGTH_SHORT).show()
+        })
 
         //Observe for error message
         flickrViewModel.errorHandleMessage.observe(this, Observer { message ->
@@ -55,21 +59,28 @@ class PhotoListActivity : AppCompatActivity() {
                 errorSnackBar = Snackbar.make(binding.root, message, Snackbar.LENGTH_INDEFINITE)
                 errorSnackBar?.setAction(R.string.retry,flickrViewModel.errorHandlerOnClick)
                 errorSnackBar?.show()
-            }else{
-                errorSnackBar?.dismiss()
-            }
+            }else errorSnackBar?.dismiss()
         })
+
+        flickrViewModel.title.observe(this, Observer { title -> if(title != null) binding.toolbar.title = title })
 
         binding.viewModel = flickrViewModel
 
     }
 
     fun validateString(string: String?): Boolean{
-        if (string == null){
+        if (string == ""){
             Toast.makeText(binding.root.context,"Please Enter a Valid Text!!", Toast.LENGTH_SHORT).show()
-            return false;
+            return false
         }
         return true
+    }
+
+
+    fun checkForConnection() : Boolean{
+        val cm = applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
+        return activeNetwork?.isConnectedOrConnecting == true
     }
 
 }
