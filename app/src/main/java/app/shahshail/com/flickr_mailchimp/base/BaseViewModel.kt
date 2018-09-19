fe364049e699cab5105525d7b7ccfd36021256dd
Shail Shah <shahshail9898@gmail.com>
2018-09-19 01:44:46 -0400

    cleanup

--- app/src/main/java/app/shahshail/com/flickr_mailchimp/base/BaseViewModel.kt
+++ app/src/main/java/app/shahshail/com/flickr_mailchimp/base/BaseViewModel.kt
@@ -1,7 +1,6 @@
 package app.shahshail.com.flickr_mailchimp.base
 
 import android.arch.lifecycle.ViewModel
-import app.shahshail.com.flickr_mailchimp.DI.Module.NetworkModule
 import app.shahshail.com.flickr_mailchimp.DI.component.DaggerViewModelInjector
 import app.shahshail.com.flickr_mailchimp.DI.component.ViewModelInjector
 import app.shahshail.com.flickr_mailchimp.View.ViewModels.PhotoListViewModel
@@ -10,7 +9,6 @@ abstract class BaseViewModel: ViewModel() {
 //    private val injector: ViewModelInjector = DaggerViewModelInjector.
 
     private val injector: ViewModelInjector = DaggerViewModelInjector.builder()
-            .networkModule(NetworkModule)
             .build()
 
     init {
